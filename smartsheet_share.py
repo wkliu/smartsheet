77c1b4e6dbfb4bc12883bba25ce475213f59ebfa
wkliu <wkliu228@gmail.com>
2018-10-17 22:25:49 +0800

    Modified to share Sights (Dashboard) on smartsheet_share.py.

--- smartsheet_share.py
+++ smartsheet_share.py
@@ -4,17 +4,18 @@ proxies = {'http': 'http://proxy.esl.cisco.com:80/', 'https':'http://proxy.esl.c
 #ss = smartsheet.Smartsheet(access_token=access_token, proxies=proxies)
 ss = smartsheet.Smartsheet(access_token)
 SEs=[davidtai, andrewyang, stanhuang, jimcheng, karlhsieh, vincenthsu, angelalin, barryhuang, vanhsieh, rickywang, tonyhsieh, willyhuang, jerrylin, allentseng, vinceliu]
-Share_Sheets = [destination_sheetIds["mm"], destination_sheetIds["demoloan"]]
+Share_Sheets = [destination_sheetIds["db"]]
 for se in SEs:
     for item in Share_Sheets:
-        if item != "email":
-            #print(se[item], se["email"])
-            newShareObj = ss.models.Share()
-            newShareObj.email = se["email"]
-            newShareObj.access_level = "EDITOR"
-            print(newShareObj, item)
-            sheet = ss.Sheets.get_sheet(item)
-            sheet.share(newShareObj, send_email=False)
+        #print(se[item], se["email"])
+        newShareObj = ss.models.Share()
+        newShareObj.email = se["email"]
+        #newShareObj.access_level = "EDITOR"
+        newShareObj.access_level = "VIEWER"
+        print(newShareObj, item)
+        #sheet = ss.Sheets.get_sheet(item)
+        #sheet.share(newShareObj, send_email=False)
+        sight = ss.Sights.share_sight(item, newShareObj, False)
         #print(item)
 #newShareObj = ss.models.Share()
 #newShareObj.email = "angelin@cisco.com"
