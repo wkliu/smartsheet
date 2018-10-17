32f9a5e118bb911d0110652938326959f45e28cc
wkliu <wkliu228@gmail.com>
2018-10-17 20:34:40 +0800

    Modified smartsheet_share.py to batch shared sheets for SEs.

--- smartsheet_share.py
+++ smartsheet_share.py
@@ -4,15 +4,16 @@ proxies = {'http': 'http://proxy.esl.cisco.com:80/', 'https':'http://proxy.esl.c
 #ss = smartsheet.Smartsheet(access_token=access_token, proxies=proxies)
 ss = smartsheet.Smartsheet(access_token)
 SEs=[davidtai, andrewyang, stanhuang, jimcheng, karlhsieh, vincenthsu, angelalin, barryhuang, vanhsieh, rickywang, tonyhsieh, willyhuang, jerrylin, allentseng, vinceliu]
-
+Share_Sheets = [destination_sheetIds["mm"], destination_sheetIds["demoloan"]]
 for se in SEs:
-    for item in se:
+    for item in Share_Sheets:
         if item != "email":
-            print(se[item], se["email"])
+            #print(se[item], se["email"])
             newShareObj = ss.models.Share()
             newShareObj.email = se["email"]
             newShareObj.access_level = "EDITOR"
-            sheet = ss.Sheets.get_sheet(se[item])
+            print(newShareObj, item)
+            sheet = ss.Sheets.get_sheet(item)
             sheet.share(newShareObj, send_email=False)
         #print(item)
 #newShareObj = ss.models.Share()
